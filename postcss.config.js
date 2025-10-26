00858cd50f258e343e92e7e74e0da9a26886c045
Andrew Hughes <andrewhhughes@gmail.com>
2025-10-26 01:36:57 -0400

    Bug fixes from prev commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,4 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+// Bridge to the CommonJS config so PostCSS tooling that expects a .js file
+// still works when package.json sets "type": "module".
+const cfg = await import('./postcss.config.cjs');
+export default (cfg && cfg.default) ? cfg.default : cfg;
